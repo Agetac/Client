f8e5677c726aa9aa31855ed659e8e7a3878c6a85
Leiko <leiko@elfire.(none)>
2011-11-17 13:39:42 +0100

    Ajout du dossier gen/ dans le dépôt, pour pouvoir run sans que le pre compilateur Android ne refasse tout le taf + deux trois tweaks par rapport à l'utilisation des ressources textuelles dans res/value/string.xml

--- Agetac-client/src/org/agetac/TabsActivity.java
+++ Agetac-client/src/org/agetac/TabsActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.widget.TabHost;
 
 public class TabsActivity extends TabActivity {
+	
+	private static final String TAB_SITAC = "tab_sitac";
+	private static final String TAB_SOEI = "tab_soei";
+	private static final String TAB_MOYEN = "tab_moyen";
+	private static final String TAB_TAB4 = "tab_tab4";
+	private static final String TAB_TAB5 = "tab_tab5";
 
 	
 	@Override
@@ -20,31 +26,31 @@ public class TabsActivity extends TabActivity {
 		
 	    // Initialize a TabSpec for each tab and add it to the TabHost
 		tabIntent = new Intent().setClass(this, SITACActivity.class);
-	    spec = tHost.newTabSpec("SITAC").setIndicator("SITAC",
+	    spec = tHost.newTabSpec(TAB_SITAC).setIndicator(getString(R.string.sitac),
 	                      getResources().getDrawable(R.drawable.ic_tab_sitac))
 	                  .setContent(tabIntent);
 	    tHost.addTab(spec);
 	    
 	    tabIntent = new Intent().setClass(this, SOEIActivity.class);
-	    spec = tHost.newTabSpec("SOEI").setIndicator("SOEI",
+	    spec = tHost.newTabSpec(TAB_SOEI).setIndicator(getString(R.string.soei),
                 getResources().getDrawable(R.drawable.ic_tab_soei))
             .setContent(tabIntent);
 	    tHost.addTab(spec);
 	    
 	    tabIntent = new Intent().setClass(this, MoyenActivity.class);
-	    spec = tHost.newTabSpec("Moyens").setIndicator("Moyens",
+	    spec = tHost.newTabSpec(TAB_MOYEN).setIndicator(getString(R.string.moyen),
                 getResources().getDrawable(R.drawable.ic_tab_moyen))
             .setContent(tabIntent);
 	    tHost.addTab(spec);
 	    
 	    tabIntent = new Intent().setClass(this, Tab4Activity.class);
-	    spec = tHost.newTabSpec("TAB4").setIndicator("TAB4",
+	    spec = tHost.newTabSpec(TAB_TAB4).setIndicator(getString(R.string.tab4),
                 getResources().getDrawable(R.drawable.ic_tab_tab4))
             .setContent(tabIntent);
 	    tHost.addTab(spec);
 	    
 	    tabIntent = new Intent().setClass(this, Tab5Activity.class);
-	    spec = tHost.newTabSpec("TAB5").setIndicator("TAB5",
+	    spec = tHost.newTabSpec(TAB_TAB5).setIndicator(getString(R.string.tab5),
                 getResources().getDrawable(R.drawable.ic_tab_tab5))
             .setContent(tabIntent);
 	    tHost.addTab(spec);
