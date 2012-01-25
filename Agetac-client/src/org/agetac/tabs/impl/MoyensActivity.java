6b283e7f9196b074248e48d2380746997c2cad85
Gildas Dzinga <26004350@e003m02.istic.univ-rennes1.fr>
2012-01-25 18:05:09 +0100

    Changement de l'UI MoyensActivity

--- Agetac-client/src/org/agetac/tabs/impl/MoyensActivity.java
+++ Agetac-client/src/org/agetac/tabs/impl/MoyensActivity.java
@@ -1,6 +1,8 @@
 package org.agetac.tabs.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 import org.agetac.R;
 import org.agetac.model.ActionFlag;
@@ -15,7 +17,9 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
+// import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class MoyensActivity extends MyActivity implements OnItemClickListener {
@@ -25,42 +29,85 @@ public class MoyensActivity extends MyActivity implements OnItemClickListener {
 	private ItemAdapter itemAdapter;
 	private ActionFlag flag;
 	private IEntity touchedEntity;
+	
+	private ListView maListViewPerso;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.moyens);
-		
-	    GridView gridview = (GridView) findViewById(R.id.gridview);
-	    gridview.setOnItemClickListener(this);
+	    maListViewPerso = (ListView) findViewById(R.id.malistview);
+	    maListViewPerso.setOnItemClickListener(this);
 	    itemAdapter = new ItemAdapter();
-	    gridview.setAdapter(itemAdapter);
+	    maListViewPerso.setAdapter(itemAdapter);
+	    
+	  //Création de la ArrayList qui nous permettra de remplire la listView
+        List<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+ 
+        //On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> map;
+ 
+        //Création d'une HashMap pour insérer les informations du premier item de notre listView
+        map = new HashMap<String, String>();
+        //on insère un élément titre que l'on récupérera dans le textView titre créé dans le fichier affichageitem.xml
+        map.put("titre", "VSAV Cleunay");
+        //on insère un élément description que l'on récupérera dans le textView description créé dans le fichier affichageitem.xml
+        map.put("description", "VSAV Rouge?");
+        //on insère la référence à l'image (convertit en String car normalement c'est un int) que l'on récupérera dans l'imageView créé dans le fichier affichageitem.xml
+        map.put("img", String.valueOf(R.drawable.firetruck));
+        //enfin on ajoute cette hashMap dans la arrayList
+        listItem.add(map);
+ 
+        //On refait la manip plusieurs fois avec des données différentes pour former les items de notre ListView
+ 
+        map = new HashMap<String, String>();
+        map.put("titre", "FTP Janzé");
+        map.put("description", "Arrivée: 0915");
+        map.put("img", String.valueOf(R.drawable.firetruck));
+        listItem.add(map);
+ 
+        map = new HashMap<String, String>();
+        map.put("titre", "FTP xxxx");
+        map.put("description", "Arrivée: 0950");
+        map.put("img", String.valueOf(R.drawable.firetruck));
+        listItem.add(map);
+ 
+        //Création d'un SimpleAdapter qui se chargera de mettre les items présent dans notre list (listItem) dans la vue affichageitem
+        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.moyens_list_item,
+               new String[] {"img", "titre", "description"}, new int[] {R.id.img, R.id.titre, R.id.description});
+ 
+        //On attribut à notre listView l'adapter que l'on vient de créer
+        maListViewPerso.setAdapter(mSchedule);
+ 
+	    
 	}
 	
 	@Override
 	public void onItemClick(AdapterView<?> adpt, View v, int index, long l) {
-		try {
-			final AdapterView<?> adapter = adpt;
-			final int position = index;
-			AlertDialog.Builder confirmDelete = new AlertDialog.Builder(this);
-			confirmDelete.setTitle(R.string.dialog_title_deletevehicule);
-			confirmDelete.setMessage(R.string.dialog_deletevehicule);
-			confirmDelete.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					ItemAdapter itemAdpt = (ItemAdapter) adapter.getAdapter();
-					flag = ActionFlag.REMOVE;
-					touchedEntity = (IEntity) itemAdpt.getItem(position);
-					observable.setChanged();
-					observable.notifyObservers(MoyensActivity.this);
-				}
-			});
-			confirmDelete.setNegativeButton(R.string.no, null);
-			confirmDelete.show();
-			
-		} catch (ClassCastException e) {
-			android.util.Log.e(TAG, e.getMessage());
-		}
+////		try {/*
+//			final AdapterView<?> adapter = adpt;
+//			final int position = index;
+//			AlertDialog.Builder confirmDelete = new AlertDialog.Builder(this);
+//			confirmDelete.setTitle(R.string.dialog_title_deletevehicule);
+//			confirmDelete.setMessage(R.string.dialog_deletevehicule);
+//			confirmDelete.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+//				@Override
+//				public void onClick(DialogInterface dialog, int which) {
+//					ItemAdapter itemAdpt = (ItemAdapter) adapter.getAdapter();
+//					flag = ActionFlag.REMOVE;
+//					touchedEntity = (IEntity) itemAdpt.getItem(position);
+//					observable.setChanged();
+//					observable.notifyObservers(MoyensActivity.this);
+//				}
+//			});
+//			confirmDelete.setNegativeButton(R.string.no, null);
+//			confirmDelete.show();*/
+//			
+//			
+//			
+////		} catch (ClassCastException e) {
+////			android.util.Log.e(TAG, e.getMessage());
+////		}*/
 	}
 	
 	@Override
